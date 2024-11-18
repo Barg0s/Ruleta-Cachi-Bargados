3b083b4629c0c32c65a463e5299b446dd5ed1da8
MCachinero <mcachinerobaranda.cf@iesesteveterradas.cat>
2024-11-18 17:01:35 +0100

    tablero de juego

--- principal.py
+++ principal.py
@@ -18,7 +18,7 @@ pygame.init()
 clock = pygame.time.Clock()
 rule = [0, 32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27,13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 31, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26]
 # Definir la finestra
-screen = pygame.display.set_mode((640, 480))
+screen = pygame.display.set_mode((860, 680))
 pygame.display.set_caption('Window Title')
 mouse = {'x':-1,'y':-1}
 
